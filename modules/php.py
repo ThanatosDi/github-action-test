aa8f0aeea284e33b97e42f04ea3f3bf8bdbfe049
ThanatosDi <yykkold55tw@gmail.com>
2023-07-20 11:52:30 +0800

    fix: 修正可能沒有 .comment-ignore 檔案問題

--- modules/php.py
+++ modules/php.py
@@ -9,7 +9,10 @@ class PHP:
     def __init__(self): ...
 
     def __ignore_files(self, path: str) -> list:
-        with open(f'{os.path.join(path, ".comment-ignore")}', 'r', encoding='utf-8') as f:
+        ignore_file_path = os.path.join(path, ".comment-ignore")
+        if not os.path.exists(ignore_file_path):
+            return []
+        with open(f'{ignore_file_path}', 'r', encoding='utf-8') as f:
             return f.read().splitlines()
 
     def files(self, path: str, include_dir: list = ['tests', 'app']) -> list:
