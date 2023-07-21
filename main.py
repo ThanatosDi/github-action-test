0ea38a8b2325bb6bb388e4f5bff79a33571edb4f
古丁丁 <yykkold55tw@gmail.com>
2023-07-21 10:57:12 +0800

    z

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import json
 import os
-import pprint
 import sys
 
 import zhconv
 from alive_progress import alive_it
 
 from modules.php import PHP
-
-# from modules.python import Python # 暫時註解掉，因為還沒實現
+# from modules.python import Python
 
 
 def is_traditional(text: str, custom_dictionary: dict = {}) -> bool:
@@ -18,6 +16,7 @@ def is_traditional(text: str, custom_dictionary: dict = {}) -> bool:
     traditional_text = zhconv.convert(text, 'zh-tw', dictionary)
     return traditional_text == text
 
+
 def suggestion(text: str, custom_dictionary: dict = {}) -> str:
     with open('./dictionary.json', 'r', encoding='utf-8') as f:
         dictionary = json.loads(f.read())
@@ -25,15 +24,17 @@ def suggestion(text: str, custom_dictionary: dict = {}) -> str:
     traditional_text = zhconv.convert(text, 'zh-tw', dictionary)
     return traditional_text
 
+
 def main():
     CODE_LANGUAGE = os.environ.get('code-language', 'php')
-    ROOT_PATH = os.environ.get('root-path', '../')  # TODO: 正式時要修改路徑為 ../
-    DICTIONARY = json.loads(os.environ.get('dictionary', "{}"))
-    INCLUDE_DIR = os.environ.get(
-        'include_dir', 'tests, app').replace(' ', '').split(',')
+    ROOT_PATH = os.environ.get('root-path', './')  # TODO: 正式時要修改路徑為 ../
+    CUSTOM_DICTIONARY = dict((key, value)
+                      for key, value in tuple(os.environ.get('custom-dictionary', "")))
+    SCAN_DIR = os.environ.get(
+        'scan-dir', 'tests, app').replace(' ', '').split(',')
     ERROR_COMMENTS = []
 
-    files = PHP().files(path=ROOT_PATH, include_dir=INCLUDE_DIR)
+    files = PHP().files(path=ROOT_PATH, include_dir=SCAN_DIR)
     comments = []
     for file in alive_it(files):
         match(CODE_LANGUAGE.lower()):
@@ -43,14 +44,15 @@ def main():
             case _:
                 raise ValueError('Not support this language')
         for comment in comments:
-            if not is_traditional(comment, custom_dictionary=DICTIONARY):
+            if not is_traditional(comment, custom_dictionary=CUSTOM_DICTIONARY):
                 ERROR_COMMENTS.append({os.path.basename(file): comment})
     if any(ERROR_COMMENTS):
-        print(f"::group::發現錯誤")
+        print(f"::group::發現錯誤")  # NOSONAR
         for comment in ERROR_COMMENTS:
             for filename, comment in comment.items():
-                print(f"::error file={filename}:: {filename}: {comment} ({suggestion(comment)})")
-        print(f"::endgroup::")
+                print(
+                    f"::error file={filename}:: {filename}: {comment} ({suggestion(comment)})")
+        print(f"::endgroup::")  # NOSONAR
         sys.exit(1)
 
 
